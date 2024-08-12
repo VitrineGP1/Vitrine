42bd332404d4e3655a09cf69e5803d53917ca659
Matheus <projetoprit@gmail.com>
2024-08-12 14:28:27 -0300

    edição

---
+++ app/public/routes/router.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+
+
+router.get("/", function (req, res) {
+    res.render("pages/home", )
+});
+
+router.get("/categorias", function (req, res) {
+    res.render("pages/categorias", )
+});
+
+router.get("/minhas-compras", function (req, res) {
+    res.render("pages/minhas-compras", )
+});
+
+router.get("/nossas-ofertas", function (req, res) {
+    res.render("pages/nossas-ofertas", )
+});
+
+router.get("/suas-vendas", function (req, res) {
+    res.render("pages/suas-vendas", )
+});
+
+router.get("/pagamentos", function (req, res) {
+    res.render("pages/pagamentos", )
+});
+
+router.get("/compra-segura", function (req, res) {
+    res.render("pages/compra-segura", )
+});
+
+router.get("/mais-vendidos", function (req, res) {
+    res.render("pages/mais-vendidos", )
+});
+
+module.exports = router;
