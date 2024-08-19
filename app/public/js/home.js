22555ac9d273c37984fb84ca6d74ec7f6229e99b
matthsvieira <165339264+matthsvieira@users.noreply.github.com>
2024-08-19 20:47:23 -0300

    vcevr

--- app/public/js/home.js
+++ app/public/js/home.js
@@ -13,3 +13,28 @@ window.addEventListener('scroll', function() {
     }
     lastScrollTop = scrollTop;
 });
+
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slidesContainer = document.querySelector('.slides');
+    const totalSlides = slides.length;
+
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+
+    slidesContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+setInterval(nextSlide, 3000);
