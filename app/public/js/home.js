57ded3ec2664c21beecfe63b1ee4928531678a5d
matthsvieira <165339264+matthsvieira@users.noreply.github.com>
2024-11-01 22:14:00 -0300

    js

--- app/public/js/home.js
+++ app/public/js/home.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
     $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
