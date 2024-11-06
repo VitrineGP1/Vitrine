62d67e766e525f2ba799db6ac59e811dec85d4a8
matthsvieira <165339264+matthsvieira@users.noreply.github.com>
2024-11-05 22:17:29 -0300

    carrinho

--- app/routes/router.js
+++ app/routes/router.js
@@ -10,32 +10,12 @@ router.get("/home-perfil", function (req, res) {
     res.render("pages/home-perfil", )
 });
 
-router.get("/categorias", function (req, res) {
-    res.render("pages/categorias", )
-});
-
-router.get("/minhas-compras", function (req, res) {
-    res.render("pages/minhas-compras", )
-});
-
-router.get("/nossas-ofertas", function (req, res) {
-    res.render("pages/nossas-ofertas", )
+router.get("/carrinho", function (req, res) {
+    res.render("pages/carrinho", )
 });
 
 router.get("/login", function (req, res) {
     res.render("pages/login", )
-});
-
-router.get("/pagamentos", function (req, res) {
-    res.render("pages/pagamentos", )
-});
-
-router.get("/compra-segura", function (req, res) {
-    res.render("pages/compra-segura", )
-});
-
-router.get("/mais-vendidos", function (req, res) {
-    res.render("pages/mais-vendidos", )
 });
 
 router.get("/cadastro", function (req, res) {
