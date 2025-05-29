bb6c5177f205f35a8fcd7df14a484db03a038369
matthsvieira <165339264+matthsvieira@users.noreply.github.com>
2025-05-28 21:52:02 -0300

    we

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -184,3 +184,44 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+loginForm.addEventListener('submit', function(event) {
+      event.preventDefault(); // Impede o envio do formulário até a validação
+  
+      // Obtendo os valores dos campos
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+  
+      // Limpando mensagens de erro
+      document.getElementById('emailError').textContent = '';
+      document.getElementById('passwordError').textContent = '';
+  
+      let hasError = false;
+  
+      // Validação de email
+      if (!email) {
+        document.getElementById('emailError').textContent = 'Por favor, insira seu email.';
+        hasError = true;
+      } else if (!validateEmail(email)) {
+        document.getElementById('emailError').textContent = 'Email inválido.';
+        hasError = true;
+      }
+  
+      // Validação de senha
+      if (!password) {
+        document.getElementById('passwordError').textContent = 'Por favor, insira sua senha.';
+        hasError = true;
+      } else if (password.length < 8) {
+        document.getElementById('passwordError').textContent = 'A senha deve ter pelo menos 8 caracteres.';
+        hasError = true;
+      }
+  
+      // Se não houver erros, redirecione para o link desejado
+      if (!hasError) {        
+        // Redireciona após a validação
+        window.location.href = '/home-perfil'; // Insira o link que deseja redirecionar
+        
+        // Limpa o formulário após o envio (opcional, se o formulário estiver sendo enviado para outra página)
+        loginForm.reset();
+      }
+    });
