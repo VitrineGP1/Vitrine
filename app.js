50d393d0507a2abcc198aa5694f1f9126f7c5181
matthsvieira <165339264+matthsvieira@users.noreply.github.com>
2025-09-27 22:12:37 -0300

    Pool app

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, 'app', 'views'));
 
 // Importa o pool de conexões
-const pool = require("config/database");
+const pool = require("config/pool_conexoes");
 
 // Importa e configura as rotas de AUTENTICAÇÃO
 const authRoutes = require("routes/authRoutes")(pool);
