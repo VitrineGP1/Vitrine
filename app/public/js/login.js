62486c68a29f93290110641a6d829c9c4ac929da
matthsvieira <165339264+matthsvieira@users.noreply.github.com>
2025-10-16 00:16:57 -0300

    login

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -9,12 +9,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const passwordErrorSpan = document.getElementById('passwordError');
 
     // Carregar dados salvos se existirem
+    try{
     const savedEmail = localStorage.getItem('savedEmail');
     const savedPassword = localStorage.getItem('savedPassword');
     if (savedEmail && savedPassword) {
         emailInput.value = savedEmail;
         passwordInput.value = savedPassword;
         rememberMeCheckbox.checked = true;
+
+    } catch (error) {
+        console.warn('localStorage access blocked:', error);
     }
 
     const API_LOGIN_URL = '/api/login_usuario';
